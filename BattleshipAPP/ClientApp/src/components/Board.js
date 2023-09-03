866b362121e44845dd47d3501ce5921f6b624f91
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-09-03 14:20:33 +0200

    added board and change color tile on click

--- BattleshipAPP/ClientApp/src/components/Board.js
+++ BattleshipAPP/ClientApp/src/components/Board.js
@@ -1,18 +1,37 @@
-﻿import React from 'react';
-import Rect from './Rect';
-
+﻿import React, { useState } from 'react';
 
 const Board = () => {
+    const [board, setBoard] = useState(Array(10).fill(Array(10).fill('white')));
+
+    const changeColor = (rowIndex, colIndex) => {
+        // Create a copy of the board with the clicked tile's color changed
+        const newBoard = board.map((row, i) =>
+            row.map((color, j) => (i === rowIndex && j === colIndex ? 'blue' : color))
+        );
+
+        setBoard(newBoard);
+    };
+
     return (
-        <div style={{
-            background: "linear-gradient(45deg, #53A658, yellow 50%, #ED564D)",
-            height: "400px",
-            width: "400px",
-            display: "flex",
-            flexWrap: "wrap"
-        }}>
-            {new Array(100).fill(1).map(n => <Rect/>)} 
+        <div>
+        
+        <div className="color-board">
+            {board.map((row, rowIndex) => (
+                <div className="row" key={rowIndex}>
+                    {row.map((color, colIndex) => (
+                        <div
+                            className="tile"
+                            key={colIndex}
+                            style={{ backgroundColor: color }}
+                            
+                        ></div>
+                    ))}
+                </div>
+            ))}
+            </div>
+            <div className="btn" style={{ width: "70px", height: "70px", backgroundColor: "red" }} onClick={() => changeColor(2, 5)}></div>
         </div>
-    )
-}
+    );
+};
+
 export default Board;

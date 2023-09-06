72d8b35fe6d5bdcf760dd597fff4f3715b791079
Daniel <86838822+Ramzes9090@users.noreply.github.com>
2023-09-06 21:51:05 +0200

    added random placing ship on board

---
+++ BattleshipAPP/Class/RandNum.cs
@@ -0,0 +1,184 @@
+﻿using System;
+namespace BattleshipAPP.Class
+{
+    public class RandNum
+    {
+        // list with already occupied values, function to check it
+        public static bool checkList(List<int[]> list, int[] tab)
+        {
+            bool res = false;
+            foreach (int[] aPart in list)
+            {
+                if (aPart[0] == tab[0] && aPart[1] == tab[1])
+                    res = true;
+            }
+            return res;
+        }
+        public static int RandNumber(int rangeMin, int rangeMax)
+        {
+            Random random = new Random();
+            int randNumber = random.Next(rangeMin, rangeMax + 1);
+
+            return randNumber;
+        }
+        // board 10x10, row: 0 - 9, column: 0-9, random position of start filed for diffrent ship
+
+        // ship on 1 square
+        public static int[] ShipOneColRow(List<int[]> occupiedColRow, bool horizontal)
+        {
+
+            int[] squareOne = new int[2];
+            if (horizontal)
+            {
+                squareOne[0] = RandNumber(0, 9);
+                squareOne[1] = RandNumber(0, 9);
+            }
+            else
+            {
+                squareOne[0] = RandNumber(0, 9);
+                squareOne[1] = RandNumber(0, 9);
+            }
+            if (!checkList(occupiedColRow, squareOne))
+            {
+                occupiedColRow.Add(squareOne);
+                return squareOne;
+            }
+            else
+            {
+                Console.WriteLine("Number occupied!");
+                ShipOneColRow(occupiedColRow, horizontal);
+            }
+            return null;
+        }
+        // ship on 2 square
+        public static int[] ShipTwoColRow(List<int[]> occupiedColRow, bool horizontal)
+        {
+            int[] squareOne = new int[2];
+            int[] squareTwo = new int[2];
+            if (horizontal)
+            {
+                squareOne[0] = RandNumber(0, 8);
+                squareOne[1] = RandNumber(0, 9);
+
+                squareTwo[0] = squareOne[0] + 1;
+                squareTwo[1] = squareOne[1];
+            }
+            else
+            {
+                squareOne[0] = RandNumber(0, 9);
+                squareOne[1] = RandNumber(0, 8);
+
+                squareTwo[0] = squareOne[0];
+                squareTwo[1] = squareOne[1] + 1;
+            }
+
+            if (!checkList(occupiedColRow, squareOne) && !checkList(occupiedColRow, squareTwo))
+            {
+                occupiedColRow.Add(squareTwo);
+                occupiedColRow.Add(squareOne);
+                return squareOne;
+            }
+            else
+            {
+                Console.WriteLine("Number occupied!");
+                ShipTwoColRow(occupiedColRow, horizontal);
+            }
+            return null;
+        }
+        // ship on 3 square
+        public static int[] ShipThreeColRow(List<int[]> occupiedColRow, bool horizontal)
+        {
+            int[] squareOne = new int[2];
+            int[] squareTwo = new int[2];
+            int[] squareThree = new int[2];
+            if (horizontal)
+            {
+                squareOne[0] = RandNumber(0, 7);
+                squareOne[1] = RandNumber(0, 9);
+
+                squareTwo[0] = squareOne[0] + 1;
+                squareTwo[1] = squareOne[1];
+
+                squareThree[0] = squareOne[0] + 2;
+                squareThree[1] = squareOne[1];
+            }
+            else
+            {
+                squareOne[0] = RandNumber(0, 9);
+                squareOne[1] = RandNumber(0, 7);
+
+                squareTwo[0] = squareOne[0];
+                squareTwo[1] = squareOne[1] + 1;
+
+                squareThree[0] = squareOne[0];
+                squareThree[1] = squareOne[1] + 2;
+            }
+
+            if (!checkList(occupiedColRow, squareOne) && !checkList(occupiedColRow, squareTwo)
+                && !checkList(occupiedColRow, squareThree))
+            {
+                occupiedColRow.Add(squareThree);
+                occupiedColRow.Add(squareTwo);
+                occupiedColRow.Add(squareOne);
+                return squareOne;
+            }
+            else
+            {
+                Console.WriteLine("Number occupied!");
+                ShipThreeColRow(occupiedColRow, horizontal);
+            }
+            return null;
+        }
+        public static int[] ShipFourColRow(List<int[]> occupiedColRow, bool horizontal)
+        {
+            int[] squareOne = new int[2];
+            int[] squareTwo = new int[2];
+            int[] squareThree = new int[2];
+            int[] squareFour = new int[2];
+            if (horizontal)
+            {
+                squareOne[0] = RandNumber(0, 6);
+                squareOne[1] = RandNumber(0, 9);
+
+                squareTwo[0] = squareOne[0] + 1;
+                squareTwo[1] = squareOne[1];
+
+                squareThree[0] = squareOne[0] + 2;
+                squareThree[1] = squareOne[1];
+
+                squareFour[0] = squareOne[0] + 3;
+                squareFour[1] = squareOne[1];
+            }
+            else
+            {
+                squareOne[0] = RandNumber(0, 9);
+                squareOne[1] = RandNumber(0, 6);
+
+                squareTwo[0] = squareOne[0];
+                squareTwo[1] = squareOne[1] + 1;
+
+                squareThree[0] = squareOne[0];
+                squareThree[1] = squareOne[1] + 2;
+
+                squareFour[0] = squareOne[0];
+                squareFour[1] = squareOne[1] + 3;
+            }
+
+            if (!checkList(occupiedColRow, squareOne) && !checkList(occupiedColRow, squareTwo)
+                && !checkList(occupiedColRow, squareThree) && !checkList(occupiedColRow,squareFour))
+            {
+                occupiedColRow.Add(squareFour);
+                occupiedColRow.Add(squareThree);
+                occupiedColRow.Add(squareTwo);
+                occupiedColRow.Add(squareOne);
+                return squareOne;
+            }
+            else
+            {
+                Console.WriteLine("Number occupied!");
+                ShipFourColRow(occupiedColRow, horizontal);
+            }
+            return null;
+        }
+    }
+}
--- BattleshipAPP/Controllers/ShipController.cs
+++ BattleshipAPP/Controllers/ShipController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using BattleshipAPP.Models;
+using BattleshipAPP.Class;
 
 namespace BattleshipAPP.Controllers
 {
@@ -7,12 +8,25 @@ namespace BattleshipAPP.Controllers
     [Route("[controller]")]
     public class ShipController : ControllerBase
     {
+
+        static bool horizontalA = false;
+        static bool horizontalB = true;
+        static bool horizontalC = false;
+        static bool horizontalD= true;
+
+        static List<int[]>  occupiedColRow = new List<int[]>();
+        static int[] startA = RandNum.ShipOneColRow(occupiedColRow, horizontalA);
+        static int[] startB = RandNum.ShipTwoColRow(occupiedColRow, horizontalB);
+        static int[] startC = RandNum.ShipThreeColRow(occupiedColRow, horizontalC);
+        static int[] startD = RandNum.ShipFourColRow(occupiedColRow, horizontalD);
+
+
         private static readonly IEnumerable<ShipModel> Ships = new[]
         {
-            new ShipModel{Id=1,ClassShip=1,ShipName = "ship-a",StartColumn = 1, StartRow = 1,LengthShip=3,IsDestroyed=false, Horizontal= true },
-            new ShipModel{Id=2,ClassShip=1,ShipName = "ship-b",StartColumn = 5, StartRow = 6,LengthShip=1,IsDestroyed=false, Horizontal= true },
-            new ShipModel{Id=3,ClassShip=1,ShipName = "ship-c",StartColumn = 2, StartRow = 8,LengthShip=4,IsDestroyed=false, Horizontal= true },
-            new ShipModel{Id=4,ClassShip=1,ShipName = "ship-d",StartColumn = 8, StartRow = 3,LengthShip=2,IsDestroyed=false, Horizontal= false },
+            new ShipModel{Id=1,ClassShip=1,ShipName = "ship-a",StartColumn = startA[0], StartRow = startA[1],LengthShip=1,IsDestroyed=false, Horizontal= horizontalA },
+            new ShipModel{Id=2,ClassShip=1,ShipName = "ship-b",StartColumn = startB[0], StartRow = startB[1],LengthShip=2,IsDestroyed=false, Horizontal= horizontalB },
+            new ShipModel{Id=3,ClassShip=1,ShipName = "ship-c",StartColumn = startC[0], StartRow = startC[1],LengthShip=3,IsDestroyed=false, Horizontal= horizontalC},
+            new ShipModel{Id=4,ClassShip=1,ShipName = "ship-d",StartColumn = startD[0], StartRow = startD[0],LengthShip=4,IsDestroyed=false, Horizontal= horizontalD },
             new ShipModel{Id=5,ClassShip=1,ShipName = "ship-e",StartColumn = 4, StartRow = 4,LengthShip=3,IsDestroyed=false, Horizontal= true },
             new ShipModel{Id=6,ClassShip=1,ShipName = "ship-f",StartColumn = 0, StartRow = 1,LengthShip=1,IsDestroyed=false, Horizontal= true },
             new ShipModel{Id=7,ClassShip=1,ShipName = "ship-g",StartColumn = 8, StartRow = 3,LengthShip=4,IsDestroyed=false, Horizontal= false },
